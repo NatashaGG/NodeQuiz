306d6dda653d4436b3b0e55c945023190d7ce335
Natasha C <natashacolletta@gmail.com>
2019-09-19 18:41:14 -0500

    created and added import mods into app

---
+++ server/app.js
@@ -0,0 +1,32 @@
+// const for all imports
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': true}));
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../dist/nodequiz')));
+app.use('/', express.static(path.join(__dirname, '../dist/nodequiz')));
+
+// Global variables
+const serverPort = 3000;
+
+/*****************************Mongoose connection strings go below this line ***********************************/
+
+/*************************** API routes go below this line *********************************/
+
+
+/**
+ * Creates an express server and listens on port 3000
+ */
+http.createServer(app).listen(serverPort, function() {
+    console.log('Application started and listening on port: ${serverPort}');
+});
