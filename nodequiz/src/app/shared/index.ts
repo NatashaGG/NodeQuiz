2046eaaaa5a361cf6c666cfda7f37f6df35963f0
Natasha W <natashacolletta@gmail.com>
2019-10-09 19:55:10 -0500

    more troubleshooting, getting /cannot get

--- nodequiz/src/app/shared/index.ts
+++ nodequiz/src/app/shared/index.ts
@@ -1,1 +1,10 @@
+/*
+=====================================
+  ; Title: index.ts
+  ; Author: Natasha Whitmer
+  ; Date: October 9 2019
+  ; Description: index.ts
+======================================
+*/
+
 export * from './base-layout/base-layout.component';
